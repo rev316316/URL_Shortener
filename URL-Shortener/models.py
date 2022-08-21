7aaacf50eef7f8a15b21191f6cc24709a7548744
Jorge <jorge.perez3@upr.edu>
2022-08-21 01:51:02 -0400

    Corrected URL Shortener

--- URL-Shortener/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-
-class placeholder(db.model):
-    id = db.Column(db.integer, primary_key=True)
-    long_url = db.Column(db.string(150), unique=True)
-    short_url = db.Column(db.string(150), unique = True)
-
-
